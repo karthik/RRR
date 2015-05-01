99cd3526edea3d98e750cf9f05753f62c04e4939
Karthik Ram <karthik.ram@gmail.com>
2015-05-01 11:27:09 -0700

    Fixed up code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-RRR project
+
+# The Reproducibility Lexicon Project
+
+We're hoping to disambiguate various terms used to describe reproducibility. For example, in some fields, Repeatability is used in place of computational reproducibility. In field sciences this is often switched around.
+
+__Rough methodology__
+
+* Search wos for these three major terms
+* Grab 10k (or max if less) records
+* Parse into manageable csv file with Publication type, authors, title, abstract, keywords, DOI.
+* Then visualize these data and go on to step 2.
+    * Some viz ideas include looking at distribution across fields, years, and possibly doing some topic modeling on the abstract keywords.
+
--- testing_parsing.R
+++ testing_parsing.R
@@ -1,21 +1,20 @@
-# bounds function
-# Used the following regular expression to replace empty lines with "---"
-#   ^(?:[\t ]*(?:\r?\n|\r))+
-# Did this in my code editor
 library(dplyr)
 library(readr)
 library(progress)
+library(parallel)
+# You must also have package called reader
+# Otherwise install.packages("reader") not to be confused with readr
 
 # Grabs the start of each record and how many lines it contains
 get_bounds <- function(file) {
-  ll <- read.fwf(file, widths=c(2,500), as.is = TRUE)
-  dividers <- which(ll[, 1] == "--")
-  start <- c(0, dividers[1:(length(dividers)-1)])
+  ll <- read.fwf(file, widths=c(2, 500), as.is = TRUE)
+  dividers <- which(ll[, 1] == "ER") # ER denotes end of record
+  start <- c(0, dividers[1:(length(dividers) - 1)])
   start <- start
   back <- dividers - 1
-  start[1] <- 0 # reset the firs to be 0
+  start[1] <- 0 # reset the first item to be 0 since we don't skip at top
   count <- back - start
-  d <- data.frame(st = start, en = back, n = count)
+  data.frame(st = start, n = count)
 }
 
 
@@ -31,20 +30,29 @@ parseEntry <- function(entry) {
   names(df) <- as.character(x[, 1])
   df
 }
-#  Pulls out each record from a giant file of records
+#  Pulls out each record from a giant file of records. The "bounds" df contains
+# how many lines to skip and how many lines to read from that point forward
 new_parse <- function(file, start, end) {
   message("file: ", file, "| start: ", start, "| end ", end, "\n")
   x <- paste(reader::n.readLines(file,skip = start, n = end), collapse = "\n")
   parseEntry(x)
 }
 
+# Basically pulls together the above two fns
+# The mapply
 meta_parse <- function(dataset, output) {
   bounds <- get_bounds(dataset)
-  res <- mapply(new_parse, file = dataset, start = bounds$st, end = bounds$n,  SIMPLIFY = FALSE)
+  res <- mapply(new_parse,
+                  file = dataset,
+                  start = bounds$st,
+                  end = bounds$n,
+                  SIMPLIFY = FALSE)
   res_df <- data.table::rbindlist(res, fill = TRUE)
   # Remove factors
-  res_df <- data.frame(res_df, stringsAsFactors = FALSE)
-  write_csv(res_df, path = output)
+  res_df <- tbl_df(data.frame(res_df, stringsAsFactors = FALSE))
+  # Pub type, author, title, source, abstract, DOI, pub year
+  res_df_clean <- res_df %>% select(PT, AU, TI, SO, AB, DI, PY)
+  write_csv(res_df_clean, path = output)
   invisible()
 }
 
